023eb51be7735475c32ecc95efc4e5aa8151004a
seonno <lsno1106@gmail.com>
2021-01-12 17:46:35 +0900

    add get customers rest api

--- client/src/App.js
+++ client/src/App.js
@@ -20,40 +20,31 @@ const styles = theme => ({
   }
 })
 
-const customers = [
-  {
-    'id': 1,
-    'image': `https://placeimg.com/64/64/1`,
-    'name': `이선노`,
-    'birthday': `961106`,
-    'sex': `남자`,
-    'job': `대학생`
-  },
-  {
-    'id': 2,
-    'image': `https://placeimg.com/64/64/2`,
-    'name': `브라이언`,
-    'birthday': `961106`,
-    'sex': `여자`,
-    'job': `백수`
-  },
-  {
-    'id': 3,
-    'image': 'https://placeimg.com/64/64/3',
-    'name': `다이아`,
-    'birthday': `961106`,
-    'sex': `남자`,
-    'job': `디자이너`
+class App extends Component {
+
+  state = {
+    customers: ""
   }
 
-];
+  componentDidMount() {
+    this.callApi()
+      .then(res => {
+        this.setState({ customers: res });
+      })
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch(`/api/customers`);
+    const body = await response.json();
+    return body;
+  }
 
 
-class App extends Component {
   render() {
     const classes = styles(createMuiTheme());
     return (
-      <Paper className={classes.root}>
+      <Paper className={classes.root} >
         <Table className={classes.table}>
           <TableHead>
             <TableRow>
@@ -68,7 +59,9 @@ class App extends Component {
             </TableRow>
           </TableHead>
           <TableBody>
-            {customers.map(c => { return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} sex={c.sex} job={c.job} />) })}
+            {this.state.customers ? this.state.customers.map(c => {
+              return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} sex={c.sex} job={c.job} />)
+            }) : ""}
           </TableBody>
         </Table>
 
